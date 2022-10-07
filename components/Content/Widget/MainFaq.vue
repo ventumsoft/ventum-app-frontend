6047bbacf4d611281434ee2546cc0ae13144a636
Developer VentumPrint <dev@ventumprint.com>
2022-10-07 18:38:45 +0300

    Fix for MainFaq widget

--- components/Content/Widget/MainFaq.vue
+++ components/Content/Widget/MainFaq.vue
@@ -4,12 +4,12 @@
       <h3 v-html="title"></h3>
     </div>
     <div class="accordion clearfix" data-state="first">
-      <template v-for="item of items">
-        <div class="acctitle">
+      <template v-for="(item, itemIndex) of items">
+        <div class="acctitle" :class="{acctitlec: currentItemIndex === itemIndex}" @click="chooseItem(itemIndex)">
           <i class="acc-closed icon-line-circle-plus"></i><i class="acc-open icon-line-circle-check color"></i>
           {{ item.question }}
         </div>
-        <div class="acc_content clearfix block-text-align-j" v-html="item.answer"></div>
+        <div ref="contents" class="acc_content clearfix block-text-align-j" v-html="item.answer"></div>
       </template>
       <TheLink :to="link" class="more-link fright">
         {{ $trans('widgets.mainthem_main_faq.all_questions') }}
@@ -24,6 +24,22 @@ export default {
     title: {type: String},
     link: {type: String},
     items: {type: Array},
+  },
+  data: () => ({
+    currentItemIndex: 0,
+  }),
+  mounted() {
+    $(this.$refs.contents).filter(':not(:eq(0))').hide();
+  },
+  methods: {
+    chooseItem(itemIndex) {
+      if (itemIndex === this.currentItemIndex) {
+        return;
+      }
+      $(this.$refs.contents[this.currentItemIndex]).slideUp();
+      this.currentItemIndex = itemIndex;
+      $(this.$refs.contents[itemIndex]).slideDown();
+    },
   },
 }
 </script>
